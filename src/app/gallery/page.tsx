5803423caa6353c3474bed78d72f95050ad661a5
Muhammad Awais <awaissoomro65@gmail.com>
2024-11-22 21:59:38 +0500

    update

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './Gallery.module.css'; // Import CSS Module
+import styles from './gallery.module.css'; // Import CSS Module
 
 export default function Gallery() {
   return (
